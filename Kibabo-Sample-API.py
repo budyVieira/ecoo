f446d46658a657d02d57a06531e18bf9742be7f6
budyVieira <54555354+budyVieira@users.noreply.github.com>
2024-03-18 11:03:42 +0100

    Update Kibabo-Sample-API.py

--- Kibabo-Sample-API.py
+++ Kibabo-Sample-API.py
@@ -24,39 +24,13 @@ def extract_product_data(url):
 
         # Encontrar todas as divs que contêm os produtos.
         product_divs = soup.find_all("div", class_="col-sm-3 col-xs-12 column columnWidth1 column-default")
-        product_divs1 = soup.find_all("div", class_="col-sm-3 col-xs-12 column columnWidth1 first-line column-default rdc-productlist-desktop-first-row")
+        product_divs.extend(soup.find_all("div", class_="col-sm-3 col-xs-12 column columnWidth1 first-line column-default rdc-productlist-desktop-first-row"))
 
         # Criar uma lista para armazenar os dados dos produtos.
         products = []
 
         # Para cada div de produto, extrair as informações e adicionar à lista.
         for product_div in product_divs:
-            # Extrair a URL da imagem do produto.
-            image_url = product_div.find("img")["src"]
-
-            # Encontrar a div que contém o nome do produto.
-            name_div = product_div.find("div", class_="desc desc-top hidden-xl hidden-lg hidden-md hidden-sm")
-
-            # Extrair o nome do produto.
-            product_name = name_div.find("p", class_="name").text.strip()
-            product_name1 = name_div.find("p", class_="key1-name").text.strip()
-
-            # Encontrar a div que contém o preço do produto.
-            price_div = product_div.find("div", class_="price dis-table clearfix")
-
-            # Extrair o preço do produto.
-            product_price = price_div.find("p", class_="current").text.strip()
-
-            # Adicionar as informações do produto à lista.
-            products.append({
-                "image_url": image_url,
-                "name": product_name1,
-                "name1": product_name,
-                "price": product_price
-            })
-
-        # Para cada div de produto, extrair as informações e adicionar à lista.
-        for product_div in product_divs1:
             # Extrair a URL da imagem do produto.
             image_url = product_div.find("img")["src"]
 
